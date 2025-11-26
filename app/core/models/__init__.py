29cad38719d9f39810306c53b88b81f7cab2fde5
vladg-pc <gricenkovlad007@gmail.com>
2025-11-26 16:07:03 +0300

    Create first schema for user

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -1,8 +1,4 @@
-__all__ = (
-    "db_helper",
-    "Base",
-    "User"
-)
+__all__ = ("db_helper", "Base", "User")
 
 from app.core.models.db_helper import db_helper
 from app.core.models.base import Base
