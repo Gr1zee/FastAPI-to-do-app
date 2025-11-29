2b48b493bf1ca47de0a5659120d6b57312bcb538
vladg-note <gricenkovlad007@gmail.com>
2025-11-29 13:24:50 +0300

    Delete unneccesary files

--- app/tasks/views.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/tasks")
-
-
-@router.get("/")
-def get_tasks():
-    return {"task_1": {"title", "descripton", "priority"}}
--- app/users/crud.py
+++
@@ -1,9 +0,0 @@
-from app.users.schemas import CreateUser
-
-
-def create_user(user_in: CreateUser):
-    user = user_in.model_dump()
-    return {
-        "success": True,
-        "user": user,
-    }
