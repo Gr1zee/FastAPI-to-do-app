726a5befe4a5e066e7373b6cbe950a6a7086742d
vladg-note <gricenkovlad007@gmail.com>
2025-11-30 15:59:53 +0300

    fix api include

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.core.config import settings
 from contextlib import asynccontextmanager
 
 from app.core.models import db_helper, Base
-from app.api.api_v1 import router as api_v1_router
+from app.api import router as api_roter
 
 
 @asynccontextmanager
@@ -18,10 +18,9 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(prefix=settings.api.prefix, router=api_v1_router)
+app.include_router(prefix=settings.api.prefix, router=api_roter)
 
 if __name__ == "__main__":
-    # When running this file directly with python, use this path
     uvicorn.run(
         "app.main:app", host=settings.run.host, port=settings.run.port, reload=True
     )
